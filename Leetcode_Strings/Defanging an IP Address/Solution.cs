11720de6509aa480fe307e1b6891a7cc52d35c74
Mark <markejay.am@gmail.com>
2021-05-18 00:48:10 -0400

    LeetCode

--- Leetcode_Strings/Defanging an IP Address/Solution.cs
+++ Leetcode_Strings/Defanging an IP Address/Solution.cs
@@ -6,5 +6,10 @@ namespace Leetcode_Strings.Defanging_an_IP_Address
 {
     class Solution
     {
+        public string DefangIPaddr(string address)
+        {
+            string output = address.Replace(".", "[.]");
+            return output;
+        }
     }
 }
--- Leetcode_Strings/Program.cs
+++ Leetcode_Strings/Program.cs
@@ -6,7 +6,9 @@ namespace Leetcode_Strings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*string address = "255.100.50.0";
+            Defanging_an_IP_Address.Solution solution = new Defanging_an_IP_Address.Solution();
+            Console.WriteLine(solution.DefangIPaddr(address));*/
         }
     }
 }
